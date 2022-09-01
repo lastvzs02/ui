f10ba41b6986fc12f31b20f8fd44783756bbd1bf
MrDipper <Naruhina5>
2022-09-01 14:26:44 +0500

    new words

--- main.py
+++ main.py
@@ -69,12 +69,14 @@ def callback_worker(call):
         resolutions = available_resolutions(some_data[2])
 
         if call.data == "video":
-            bot.edit_message_text("Выберите рвзрешение", call.message.chat.id, call.message.message_id,
+            bot.edit_message_text(lang[db_conn.get_user_lang(call.message.chat.id)]['Choose_resolution'],
+                                  call.message.chat.id, call.message.message_id,
                                   reply_markup=choose_resolution(call.message, some_data[2]))
             db_conn.set_content_type(call.message.chat.id, 'video')
 
         elif call.data == "audio":
-            wait_mess = bot.edit_message_text("Подождите...", call.message.chat.id, call.message.message_id)
+            wait_mess = bot.edit_message_text(f"{lang[db_conn.get_user_lang(call.message.chat.id)]['Wait']}...",
+                                              call.message.chat.id, call.message.message_id)
             db_conn.set_content_type(call.message.chat.id, 'audio')
             audio_path, audio_title = downloader_audio(some_data[2])
             bot.send_audio(call.message.chat.id, open(audio_path, 'rb'), caption=audio_title)
@@ -82,7 +84,8 @@ def callback_worker(call):
             os.remove(audio_path)
 
         elif call.data in resolutions:
-            wait_mess = bot.edit_message_text("Подождите...", call.message.chat.id, call.message.message_id)
+            wait_mess = bot.edit_message_text(f"{lang[db_conn.get_user_lang(call.message.chat.id)]['Wait']}...",
+                                              call.message.chat.id, call.message.message_id)
             video_path, video_title = downloader_video(some_data[2], quality=call.data)
             bot.send_document(call.message.chat.id, open(video_path, 'rb'), caption=video_title)
             bot.delete_message(call.message.chat.id, wait_mess.message_id)
