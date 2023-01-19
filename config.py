ed7093876f157127e4d76e716eebbb86380c036b
lastvzs02 <121180285+lastvzs02@users.noreply.github.com>
2023-01-19 07:36:52 -0500

    Update config.py

--- config.py
+++ config.py
@@ -1,16 +1,16 @@
-TOKEN = "5329591616:AAFjrB_lIyg3IJkT0odONPYmxcR-2Giw3lQ"
+TOKEN = "5506368172:AAGuwPsf7Evc4q-7_paPhuI9wqEWi56Efr4"
 
-# HOST = "localhost"
-# DATABASE_NAME = "YT_Downloader"
-# USER = "postgres"
-# PORT = 5432
-# PASSWORD = "Naruhina"
-
-HOST = "ec2-44-209-158-64.compute-1.amazonaws.com"
-DATABASE_NAME = "d6lpnh4unaho5u"
-USER = "dgjccivklesdvt"
+HOST = "localhost"
+DATABASE_NAME = "YT_Downloader"
+USER = "postgres"
 PORT = 5432
-PASSWORD = "7cddd161180bb13acc534ebf7221eb316b725393ffb26493d722af6419f37316"
+PASSWORD = "Naruhina"
+
+#HOST = "ec2-44-209-158-64.compute-1.amazonaws.com"
+#DATABASE_NAME = "d6lpnh4unaho5u"
+#USER = "dgjccivklesdvt"
+#PORT = 5432
+#PASSWORD = "7cddd161180bb13acc534ebf7221eb316b725393ffb26493d722af6419f37316"
 
 VIDEOS_DIR = 'videos/'
 AUDIOS_DIR = 'audios/'
